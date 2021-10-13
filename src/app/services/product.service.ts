b147bc3d424b57bed0e6c4ce761bfcbcadb6721d
ldex <laurent.duveau+git@gmail.com>
2021-10-13 10:00:47 -0400

    Product List avec pagination

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -22,8 +22,27 @@ export class ProductService {
                       .get<Product[]>(this.baseUrl)
                       .pipe(
                         delay(1500), // Fake delay!!
-                        tap(console.table)
+                        tap(console.table),
+                        shareReplay(),
+                        catchError(this.handleError)
                       );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    // in a real world app, you may send the error to the server using some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMsg: string;
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMsg = 'An error occurred:' + error.error.message;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      errorMsg = `Backend returned code ${error.status}, body was: ${error.error}`;
+    }
+    console.error(errorMsg);
+    // return an observable with a user-facing error message
+    return throwError('Something bad happened; please try again later.');
   }
 
 }
